1aab938a0fc5308ae87253af9f71c360c12d7f23
SolarStreamer <kdawsonfoster@gmail.com>
2026-06-27 19:33:16 +0100

    Implement applySpellEffect function for target updates

--- engine/apply_spell_effect.js
+++ engine/apply_spell_effect.js
@@ -1,1 +1,27 @@
+export function applySpellEffect(target, spell) {
+    if (spell.dmg) {
+        target.hp -= spell.dmg;
+    }
 
+    if (spell.heal) {
+        target.hp += spell.heal;
+    }
+
+    if (spell.atkBoost) {
+        target.atk += spell.atkBoost;
+    }
+
+    if (spell.defBoost) {
+        target.def += spell.defBoost;
+    }
+
+    if (spell.damageReduction) {
+        target.tempDamageReduction = spell.damageReduction;
+    }
+
+    if (spell.energyGain) {
+        target.energy += spell.energyGain;
+    }
+
+    return target;
+}
