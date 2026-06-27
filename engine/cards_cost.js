001017c26ff0528a648a80ee341a1349cd5aa546
SolarStreamer <kdawsonfoster@gmail.com>
2026-06-27 19:33:54 +0100

    Add cardCosts object with various game costs

--- engine/cards_cost.js
+++ engine/cards_cost.js
@@ -1,1 +1,25 @@
+export const cardCosts = {
+    army: 3,
+    mixKnight: 3,
+    mixArcher: 4,
+    spellCaster: 5,
 
+    spells: {
+        small: 2,
+        medium: 3,
+        big: 4
+    },
+
+    traps: {
+        hand: 2,
+        map: 3
+    },
+
+    items: {
+        small: 2,
+        medium: 3,
+        big: 5
+    },
+
+    attack: 1
+};
