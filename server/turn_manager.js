0a56a49752f3d2818f6443a23341e490f5f9cadc
SolarStreamer <kdawsonfoster@gmail.com>
2026-06-27 19:35:15 +0100

    Implement TurnManager class with game phases

--- server/turn_manager.js
+++ server/turn_manager.js
@@ -1,1 +1,50 @@
+import { cardCosts } from "../engine/card_costs.js";
 
+export class TurnManager {
+    constructor(gameState) {
+        this.game = gameState;
+    }
+
+    startPhase() {
+        const player = this.game.getCurrentPlayerState();
+
+        // Energy gain (+1 or +2 if Energy Leader)
+        const leaderBuff = player.leader.buff;
+        player.energy += leaderBuff === "Energy+" ? 2 : 1;
+    }
+
+    drawPhase() {
+        const player = this.game.getCurrentPlayerState();
+        if (player.deck.length > 0) {
+            const card = player.deck.shift();
+            player.hand.push(card);
+        }
+    }
+
+    mainPhase() {
+        // This phase is controlled by client actions.
+        // Server only checks legality.
+        return true;
+    }
+
+    battlePhase() {
+        // Attacks handled in combat_manager.js
+        return true;
+    }
+
+    endPhase() {
+        // Cleanup temporary effects
+        const player = this.game.getCurrentPlayerState();
+        player.tempDamageReduction = 0;
+        this.game.cancelNextSpell = false;
+    }
+
+    runTurn() {
+        this.startPhase();
+        this.drawPhase();
+        this.mainPhase();
+        this.battlePhase();
+        this.endPhase();
+        this.game.switchTurn();
+    }
+}
