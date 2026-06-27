18b14732f624b569d99e86331d542864440fc41b
SolarStreamer <kdawsonfoster@gmail.com>
2026-06-27 19:36:34 +0100

    Implement EnergyManager class for energy management

--- server/energy_manager.js
+++ server/energy_manager.js
@@ -1,1 +1,22 @@
+import { cardCosts } from "../engine/card_costs.js";
 
+export class EnergyManager {
+    constructor(gameState) {
+        this.game = gameState;
+    }
+
+    canAfford(player, cost) {
+        return player.energy >= cost;
+    }
+
+    spendEnergy(player, cost) {
+        if (!this.canAfford(player, cost)) return false;
+        player.energy -= cost;
+        return true;
+    }
+
+    payForCard(player, cardType) {
+        const cost = cardCosts[cardType];
+        return this.spendEnergy(player, cost);
+    }
+}
