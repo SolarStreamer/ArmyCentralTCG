af569329d206000450c51a3a5c18167eb311fdb3
SolarStreamer <kdawsonfoster@gmail.com>
2026-06-27 19:33:37 +0100

    Implement applyTrapEffect function for game mechanics

--- engine/apply_trap_effect.js
+++ engine/apply_trap_effect.js
@@ -1,1 +1,32 @@
+export function applyTrapEffect(target, trap, gameState) {
+    switch (trap.effect) {
+        case "stunNextTurn":
+            target.stunned = true;
+            break;
 
+        case "forceDefPosition":
+            target.position = "DEF";
+            break;
+
+        case "lose1Energy":
+            target.energy = Math.max(0, target.energy - 1);
+            break;
+
+        case "cancelNextSpell":
+            gameState.cancelNextSpell = true;
+            break;
+
+        case "revealFaceDown":
+            target.revealed = true;
+            break;
+
+        case "revealHand":
+            target.handRevealed = true;
+            break;
+
+        default:
+            break;
+    }
+
+    return { target, gameState };
+}
