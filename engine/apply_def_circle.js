17c12df361203f015436b39113f1dbac3a58aeb4
SolarStreamer <kdawsonfoster@gmail.com>
2026-06-27 19:32:30 +0100

    Add getDefenseValue function for unit defense calculation

--- engine/apply_def_circle.js
+++ engine/apply_def_circle.js
@@ -1,1 +1,8 @@
+export function getDefenseValue(unitRace, enemyRace, defCircle) {
+    const matchup = defCircle[unitRace];
 
+    if (enemyRace === matchup.strongAgainst) return matchup.strongDef;
+    if (enemyRace === matchup.weakAgainst) return matchup.weakDef;
+
+    return matchup.neutralDef;
+}
