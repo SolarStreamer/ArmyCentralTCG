2ca6184c3dd68ec9352f839f4cc92fdbb93f2025
SolarStreamer <kdawsonfoster@gmail.com>
2026-06-27 19:32:58 +0100

    Add applyItemBuff function to apply item effects

--- engine/apply_item_buff.js
+++ engine/apply_item_buff.js
@@ -1,1 +1,7 @@
+export function applyItemBuff(unit, item) {
+    if (item.atkBoost) unit.atk += item.atkBoost;
+    if (item.defBoost) unit.def += item.defBoost;
+    if (item.heal) unit.hp += item.heal;
 
+    return unit;
+}
