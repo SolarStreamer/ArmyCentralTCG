af8ae39c74908a596197ba24d4a2d224e32fd34b
SolarStreamer <kdawsonfoster@gmail.com>
2026-06-27 19:32:09 +0100

    Add calculateDamage function for combat mechanics

--- engine/calculate_damage.js
+++ engine/calculate_damage.js
@@ -1,1 +1,15 @@
+export function calculateDamage(attacker, defender, defCircle) {
+    const race = defender.race;
+    const matchup = defCircle[race];
 
+    let defValue = matchup.neutralDef;
+
+    if (attacker.race === matchup.strongAgainst) {
+        defValue = matchup.weakDef;
+    } else if (attacker.race === matchup.weakAgainst) {
+        defValue = matchup.strongDef;
+    }
+
+    const rawDamage = attacker.atk - defValue;
+    return rawDamage > 0 ? rawDamage : 0;
+}
